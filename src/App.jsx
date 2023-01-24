b28585464622f3240d9e1f1448404d38543c12ee
Edgar Méndez <mdzredgar@gmail.com>
2023-01-23 21:05:55 -0600

    Usando catch en el ejemplo anterior.

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,7 @@ function App() {
     fetch("https://randomuser.me/api/")
       .then((response) => response.json())
       .then((data) => console.log(data))
+      .catch((err) => console.log(err))
     
   }
 
